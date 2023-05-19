aea6ad360131129d643958fe5560fa79b8b49c2a
Diego Alfonso <dlalfonso57@hotmail.com>
2023-05-18 23:07:55 -0500

    componentes funcionales

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ClassComponent from "./components/ClassComponents";
+import FuncComponent from "./components/FuncComponents";
+
+function App() {
+  return (
+    <div>
+      <ClassComponent />
+      <FuncComponent />
+    </div>
+  );
+}
+
+export default App;
