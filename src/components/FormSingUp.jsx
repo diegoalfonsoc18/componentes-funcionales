494d454335cea992623f4f06c82520d1c64a330e
Diego Alfonso <dlalfonso57@hotmail.com>
2023-05-21 18:20:41 -0500

    button with material-ui

--- src/components/FormSingUp.jsx
+++ src/components/FormSingUp.jsx
@@ -1,8 +1,38 @@
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+
 function FormSingUp() {
   return (
     <form>
-      <input type="text" />
-      <button>Registrarse</button>
+      <TextField
+        id="name"
+        label="Nombre"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="lastN"
+        label="Apellidos"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="email"
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+
+      <label>Promociones</label>
+      <input type="checkbox" />
+      <label>Novedades</label>
+      <input type="checkbox" />
+      <Button variant="contained" color="success">
+        Registrarse
+      </Button>
     </form>
   );
 }
